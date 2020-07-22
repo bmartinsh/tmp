2c2b481f46714ebc25455e81d718aa9b46f78a7f
benjamin martin <benjamin@martin-zhu.com>
2020-07-22 23:50:35 +0800

    TOUTIAO with BAAS READ ALL COMMENTS

--- toutiao/pages/index/index.js
+++ toutiao/pages/index/index.js
@@ -36,16 +36,16 @@ Page({
     console.log(Story)
     
     // To uncomment to see all stories
-    // Story.find().then((res) => {
-    //   console.log('res', res);
-    //   this.setData({
-    //     stories: res.data.objects
+    Story.find().then((res) => {
+      console.log('res', res);
+      this.setData({
+        stories: res.data.objects
 
-        // Here to see one story with function GET
-        Story.get('5f16df23e1b0574b06eef6dc').then((res) => {
-          console.log('res', res);
-          this.setData({
-            stories: res.data.objects
+        // // Here to see one story with function GET
+        // Story.get('5f16df23e1b0574b06eef6dc').then((res) => {
+        //   console.log('res', res);
+        //   this.setData({
+        //     stories: res.data.objects
 
       })
     })
