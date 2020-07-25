d89492dfd94da5d4b7a66611b499108ddadce4ab
benjamin martin <benjamin@martin-zhu.com>
2020-07-25 18:02:39 +0800

    dianping create  comments

--- dianping/pages/profile/index.js
+++ dianping/pages/profile/index.js
@@ -20,13 +20,17 @@ Page({
   },
   userInfoHandler(data) {
     const app = getApp();
+
     wx.BaaS.auth.loginWithWechat(data).then(user => {
       console.log('user', user);
         app.globalData.userInfo = user;
         wx.setStorageSync('userInfo', user);
         this.setData({
           currentUser: user,
-        })
+        });
+        wx.navigateTo({
+          url: `/pages/index/index`
+        });
       }, err => {
         // **err 有两种情况**：用户拒绝授权，HError 对象上会包含基本用户信息：id、openid、unionid；其他类型的错误，如网络断开、请求超时等，将返回 HError 对象（详情见下方注解）
     })
